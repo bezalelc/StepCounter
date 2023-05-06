e65123baad2261dfbb1bc1956c5b5e5c87bf5037
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-05-06 16:46:39 +0300

    Login Register Splash Setting Main

--- app/src/main/java/com/yehonatand_bezalelc/stepcounter/MainActivity.java
+++ app/src/main/java/com/yehonatand_bezalelc/stepcounter/MainActivity.java
@@ -1,14 +1,49 @@
 package com.yehonatand_bezalelc.stepcounter;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+        DailyFragment dailyFragment = new DailyFragment();
+        SummeryFragment summeryFragment = new SummeryFragment();
+
+        bottomNavigationView
+                .setOnItemSelectedListener(item -> {
+                    switch (item.getItemId()) {
+                        case R.id.daily:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.flFragment, new DailyFragment())
+                                    .commit();
+                            return true;
+
+                        case R.id.summery:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.flFragment, new SummeryFragment())
+                                    .commit();
+                            return true;
+                    }
+                    return false;
+                });
+        bottomNavigationView.setSelectedItemId(R.id.daily);
     }
+
+
 }
---
+++ app/src/main/java/com/yehonatand_bezalelc/stepcounter/SummeryFragment.java
@@ -0,0 +1,20 @@
+package com.yehonatand_bezalelc.stepcounter;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SummeryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summery, container, false);
+    }
+}
