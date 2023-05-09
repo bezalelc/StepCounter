71830ce9adc1910f79f361ec73687152bd03a2ea
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-05-09 21:19:30 +0300

    Change home page to activity

--- app/src/main/java/com/yehonatand_bezalelc/stepcounter/MainActivity.java
+++ app/src/main/java/com/yehonatand_bezalelc/stepcounter/MainActivity.java
@@ -18,11 +18,11 @@ import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
-    private enum MenuItemType {
-        about,
-        setting,
-        exit
-    }
+//    private enum MenuItemType {
+//        about,
+//        setting,
+//        exit
+//    }
 
     BottomNavigationView bottomNavigationView;
 
@@ -32,75 +32,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView
-                = findViewById(R.id.bottomNavigationView);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+//        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
 
-        bottomNavigationView
-                .setOnItemSelectedListener(item -> {
-                    switch (item.getItemId()) {
-                        case R.id.daily:
-                            getSupportFragmentManager()
-                                    .beginTransaction()
-                                    .replace(R.id.flFragment, new DailyFragment())
-                                    .commit();
-                            return true;
-
-                        case R.id.summery:
-                            getSupportFragmentManager()
-                                    .beginTransaction()
-                                    .replace(R.id.flFragment, new SummeryFragment())
-                                    .commit();
-                            return true;
-                    }
-                    return false;
-                });
-        bottomNavigationView.setSelectedItemId(R.id.daily);
+        // Perform item selected listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.summery:
+                    startActivity(new Intent(getApplicationContext(), SummeryActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+                case R.id.home:
+                    return true;
+            }
+            return false;
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(MenuItemType.about.toString());
-        menu.add(MenuItemType.setting.toString());
-        menu.add(MenuItemType.exit.toString());
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
-        if (menuItem.getTitle().equals(MenuItemType.about.toString())) {
-//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//            builder.setTitle("About");
-//            builder.setMessage(
-//                    "Step Counter\n\nBezalel Cohen 308571207\nBezalel Cohen 308571207\n" +
-//                            "1.12.2023\n\n" +
-//                            "System\n" +
-//                            "System\n" +
-//                            "System\n" +
-//                            "System\n");
-//            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    // Code to be executed when the positive button is clicked
-//                }
-//            });
-////            builder.setNegativeButton("Negative Button", new DialogInterface.OnClickListener() {
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        menu.add(MenuItemType.about.toString());
+//        menu.add(MenuItemType.setting.toString());
+//        menu.add(MenuItemType.exit.toString());
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+//        if (menuItem.getTitle().equals(MenuItemType.about.toString())) {
+////            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+////            builder.setTitle("About");
+////            builder.setMessage(
+////                    "Step Counter\n\nBezalel Cohen 308571207\nBezalel Cohen 308571207\n" +
+////                            "1.12.2023\n\n" +
+////                            "System\n" +
+////                            "System\n" +
+////                            "System\n" +
+////                            "System\n");
+////            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 ////                @Override
 ////                public void onClick(DialogInterface dialog, int which) {
-////                    // Code to be executed when the negative button is clicked
+////                    // Code to be executed when the positive button is clicked
 ////                }
 ////            });
-//
-//            AlertDialog alertDialog = builder.create();
-//            alertDialog.show();
-            Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-        } else if (menuItem.getTitle().equals(MenuItemType.setting.toString())) {
+//////            builder.setNegativeButton("Negative Button", new DialogInterface.OnClickListener() {
+//////                @Override
+//////                public void onClick(DialogInterface dialog, int which) {
+//////                    // Code to be executed when the negative button is clicked
+//////                }
+//////            });
+////
+////            AlertDialog alertDialog = builder.create();
+////            alertDialog.show();
 //            Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-        } else if (menuItem.getTitle().equals(MenuItemType.exit.toString())) {
-            Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-        }
-        return super.onOptionsItemSelected(menuItem);
-    }
+//        } else if (menuItem.getTitle().equals(MenuItemType.setting.toString())) {
+////            Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+//            Intent intent = new Intent(this, SettingsActivity.class);
+//            startActivity(intent);
+//        } else if (menuItem.getTitle().equals(MenuItemType.exit.toString())) {
+//            Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+//        }
+//        return super.onOptionsItemSelected(menuItem);
+//    }
 }
