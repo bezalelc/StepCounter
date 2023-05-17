d1da43c598a8ce69692496788cff17929432cea2
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-05-17 23:09:44 +0300

    Register

--- app/src/main/java/com/yehonatand_bezalelc/stepcounter/RegisterActivity.java
+++ app/src/main/java/com/yehonatand_bezalelc/stepcounter/RegisterActivity.java
@@ -2,22 +2,14 @@ package com.yehonatand_bezalelc.stepcounter;
 
 import android.util.Patterns;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -25,8 +17,6 @@ import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout textInputLayoutEmail, textInputLayoutHeight, textInputLayoutWeight, textInputLayoutPassword;
-    private Button buttonRegister;
-    private TextView textViewLogin;
     DBPipeline dbPipeline;
 
     FirebaseAuth mAuth;
@@ -53,41 +43,40 @@ public class RegisterActivity extends AppCompatActivity {
         textInputLayoutHeight = findViewById(R.id.register_text_input_layout_height);
         textInputLayoutWeight = findViewById(R.id.register_text_input_layout_weight);
         textInputLayoutPassword = findViewById(R.id.register_text_input_layout_password);
-
+        Button buttonRegister = findViewById(R.id.register_button_register);
         dbPipeline = new DBPipeline();
         /*mAuth = FirebaseAuth.getInstance();*/
         mAuth = dbPipeline.getmAuth();
 
 
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String email = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString().trim();
-                String height = Objects.requireNonNull(textInputLayoutHeight.getEditText()).getText().toString().trim();
-                String weight = Objects.requireNonNull(textInputLayoutWeight.getEditText()).getText().toString().trim();
-                String password = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim();
+        buttonRegister.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(textInputLayoutEmail.getEditText()).getText().toString().trim();
+            String height = Objects.requireNonNull(textInputLayoutHeight.getEditText()).getText().toString().trim();
+            String weight = Objects.requireNonNull(textInputLayoutWeight.getEditText()).getText().toString().trim();
+            String password = Objects.requireNonNull(textInputLayoutPassword.getEditText()).getText().toString().trim();
 
-
-                if (!(confirmEmail(email) && confirmHeight(height) && confirmWeight(weight) && confirmPassword(password))) {
-                    return;
-                }
-
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this, "Account created.",
-                                        Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                textInputLayoutPassword.setError("Authentication failed");
-                            }
-                        });
+            boolean confirmEmail_ = confirmEmail(email), confirmHeight_ = confirmHeight(height),
+                    confirmWeight_ = confirmWeight(weight), confirmPassword_ = confirmPassword(password);
+            if (!(confirmEmail_ && confirmHeight_ && confirmWeight_ && confirmPassword_)) {
+                return;
             }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Account created.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            textInputLayoutPassword.setError("Authentication failed");
+                        }
+                    });
         });
 
-        textViewLogin = findViewById(R.id.register_button_have_account);
+        TextView textViewLogin = findViewById(R.id.register_button_have_account);
         textViewLogin.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
@@ -103,45 +92,49 @@ public class RegisterActivity extends AppCompatActivity {
             textInputLayoutEmail.setError("Please enter a valid email address");
             return false;
         }
+        textInputLayoutEmail.setError("");
         return true;
     }
 
     private boolean confirmPassword(String password) {
         if (password.equals("")) {
-            textInputLayoutEmail.setError("Field can't be empty");
+            textInputLayoutPassword.setError("Field can't be empty");
             return false;
         } else if (password.length() < 6) {
-            textInputLayoutEmail.setError("Please enter password at least 6 characters long");
+            textInputLayoutPassword.setError("Please enter password at least 6 characters long");
             return false;
         }
+        textInputLayoutPassword.setError("");
         return true;
     }
 
     private boolean confirmHeight(String height) {
         if (height.equals("")) {
-            textInputLayoutEmail.setError("Enter your height");
+            textInputLayoutHeight.setError("Enter your height");
             return false;
         } else if (Integer.parseInt(height) < 40) {
-            textInputLayoutEmail.setError("Height must be > 40");
+            textInputLayoutHeight.setError("Height must be > 40");
             return false;
         } else if (Integer.parseInt(height) > 230) {
-            textInputLayoutEmail.setError("Height must be < 230");
+            textInputLayoutHeight.setError("Height must be < 230");
             return false;
         }
+        textInputLayoutHeight.setError("");
         return true;
     }
 
     private boolean confirmWeight(String weight) {
         if (weight.equals("")) {
-            textInputLayoutEmail.setError("Field can't be empty");
+            textInputLayoutWeight.setError("Enter your weight");
             return false;
         } else if (Integer.parseInt(weight) < 30) {
-            textInputLayoutEmail.setError("Weight must be > 30");
+            textInputLayoutWeight.setError("Weight must be > 30");
             return false;
         } else if (Integer.parseInt(weight) > 300) {
-            textInputLayoutEmail.setError("Weight must be < 300");
+            textInputLayoutWeight.setError("Weight must be < 300");
             return false;
         }
+        textInputLayoutWeight.setError("");
         return true;
     }
 }
