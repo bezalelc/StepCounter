81f1c37adba0e0940cd89a717985ed41df03c3e6
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-05-27 18:51:15 +0300

    temp

--- app/src/main/java/com/yehonatand_bezalelc/stepcounter/ForgotPasswordActivity.java
+++ app/src/main/java/com/yehonatand_bezalelc/stepcounter/ForgotPasswordActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+    boolean b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
